7471f6ad19676bd195269bea42b6442f130f1b36
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-03-07 01:46:53 +0530

    worked on functionalities

--- server.js
+++ server.js
@@ -16,21 +16,21 @@ const User = require('./models/user');
 
 
 
-app.set("views",path.join(__dirname,"views"))
-app.set("view engine","ejs");
-app.engine("ejs",ejsMate);
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
 
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(session({
-    secret:"mysecretcode",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        expires: Date.now() + 7*24*60*60*1000,
-        maxAge: 7*24*60*60*1000,
-        httpOnly:true,
+    secret: "mysecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     }
 }));
 app.use(flash());
@@ -39,127 +39,127 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: "742797280040-l3vg9rbs72ea0573bnsgmmbd86c30mt5.apps.googleusercontent.com",
-    clientSecret:  "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",
+    clientSecret: "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",
     callbackURL: "http://localhost:3000/auth/google/callback",
-    scope:["email","profile"]
-    },
-    async function(request,accessToken, refreshToken, profile, done) {
-        try{
-            let user = await User.findOne({googleId:profile.id});
-            if(!user){
+    scope: ["email", "profile"]
+},
+    async function (request, accessToken, refreshToken, profile, done) {
+        try {
+            let user = await User.findOne({ googleId: profile.id });
+            if (!user) {
                 const newUser = new User({
-                googleId:profile.id,
-                displayName:profile.displayName,
-                email:profile.emails[0].value,
-                username:profile.emails[0].value,
+                    googleId: profile.id,
+                    displayName: profile.displayName,
+                    email: profile.emails[0].value,
+                    username: profile.emails[0].value,
                 });
                 user = await newUser.save();
             }
-            return done(null,user);
-        }catch(error){
-            return done(error,null);
+            return done(null, user);
+        } catch (error) {
+            return done(error, null);
         }
-}));
+    }));
 
-passport.serializeUser((user,done)=>{
-    done(null,user);
+passport.serializeUser((user, done) => {
+    done(null, user);
 })
-passport.deserializeUser((user,done)=>{
-    done(null,user);
+passport.deserializeUser((user, done) => {
+    done(null, user);
 })
 
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.currUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-main().then(()=>{console.log("connected to database")}).catch(err => console.log(err));
+main().then(() => { console.log("connected to database") }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/classroom');
-//   await mongoose.connect('mongodb+srv://as0755213:Abhi9760@cluster0.pshdmw9.mongodb.net/');
+    await mongoose.connect('mongodb://127.0.0.1:27017/classroom');
+    //   await mongoose.connect('mongodb+srv://as0755213:Abhi9760@cluster0.pshdmw9.mongodb.net/');
 }
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("app is listening at port 3000");
 })
 
-const isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()){
-        req.flash("error","You should login first !");
+const isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash("error", "You should login first !");
         return res.redirect("/");
-    }else{
+    } else {
         next();
     }
 }
 
-const isUser =  async (req,res,next)=>{
-    const {id} = req.params;
-    if(id && req.user){
+const isUser = async (req, res, next) => {
+    const { id } = req.params;
+    if (id && req.user) {
         let testUser = await test.findById(id);
-        if(testUser.owner == req.user._id){
+        if (testUser.owner == req.user._id) {
             next();
-        }else{
-            res.flash("error","You are not the owner of the test");
+        } else {
+            res.flash("error", "You are not the owner of the test");
             next(new Error("Yeh user hai hi nahi"));
         }
-    }else{
-        res.flash("error","NO test exists!!");
+    } else {
+        res.flash("error", "NO test exists!!");
         next();
     }
 }
 
-app.get('/', async (req,res)=>{
-    const tests = await Test.find({}).populate({path: 'owner'});
-    res.render('tests.ejs',{tests});
+app.get('/', async (req, res) => {
+    const tests = await Test.find({}).populate({ path: 'owner' });
+    res.render('./test/newTest.ejs', { tests });
 })
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.render('./user/login.ejs');
 })
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/auth/google/callback',passport.authenticate('google',{scope:['profile','email']}),function(req, res) {
+app.get('/auth/google/callback', passport.authenticate('google', { scope: ['profile', 'email'] }), function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/tests');
-  });
+    res.redirect('/');
+});
 
-app.post('/login',passport.authenticate("local",{failureRedirect:'/'}), (req,res)=>{
-    res.redirect('/tests');
-})
-
-
-
-app.get('/logout',(req,res)=>{
-    req.logOut((err)=>{
-        if(err){
-            console.log(err);
-        }
-    });
-    req.flash("success","Successfully Logout");
+app.post('/login', passport.authenticate("local", { failureRedirect: '/' }), (req, res) => {
     res.redirect('/');
 })
 
-app.get('/signup',(req,res)=>{
+
+
+app.get('/logout', (req, res) => {
+    req.logOut((err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    req.flash("success", "Successfully Logout");
+    res.redirect('/');
+})
+
+app.get('/signup', (req, res) => {
     res.render('./user/signup');
 })
 
-app.post('/signup', async (req,res)=>{
-    try{
-        const {username,password,email,displayName} = req.body;
-        const registerUser = await User.register({username,email,displayName},password);
-        req.login(registerUser,(err)=>{
-            if(err){
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password, email, displayName } = req.body;
+        const registerUser = await User.register({ username, email, displayName }, password);
+        req.login(registerUser, (err) => {
+            if (err) {
                 throw new Error(err);
             }
             res.redirect('/tests');
         });
-    }catch(e){
+    } catch (e) {
         console.log(e)
         res.status(400).json('something broke');
     }
@@ -171,19 +171,13 @@ app.post('/signup', async (req,res)=>{
 // all tests
 
 
-// create test
-app.get("/tests",isLoggedIn,(req,res)=>{
-    res.render("./test/newTest.ejs");
-})
-
-
-app.post('/tests', isLoggedIn,async (req,res)=>{
-    if(req.body.title == 0){
+app.post('/tests', isLoggedIn, async (req, res) => {
+    if (req.body.title == 0) {
         res.status(400).send('please provide title');
     }
-    const googleUser = await User.findOne({_id:req.user._id});
+    const googleUser = await User.findOne({ _id: req.user._id });
     let testBody = req.body;
-    testBody = {title: req.body.title,liveAt : new Date(req.body.date)};
+    testBody = { title: req.body.title, liveAt: new Date(req.body.date) };
     const newtest = new Test(testBody);
     newtest.owner = googleUser._id;
     googleUser.tests.push(newtest);
@@ -193,20 +187,20 @@ app.post('/tests', isLoggedIn,async (req,res)=>{
 })
 
 
-app.delete('/tests/:id',isLoggedIn,isUser,async(req,res)=>{
-    const {id} = req.params;
+app.delete('/tests/:id', isLoggedIn, isUser, async (req, res) => {
+    const { id } = req.params;
     let temp = await test.findByIdAndDelete(id);
     console.log(temp);
-    await User.findByIdAndUpdate(temp.owner,{$pull:{tests:id}});
+    await User.findByIdAndUpdate(temp.owner, { $pull: { tests: id } });
     console.log("deleted successfully");
     res.redirect('/');
 })
 
-app.get('/tests/:id',isLoggedIn,async (req,res)=>{
-    const {id} = req.params;
-    const test = await Test.findById(id).populate({path:'questions',populate:{path:"options"}});
+app.get('/tests/:id', isLoggedIn, async (req, res) => {
+    const { id } = req.params;
+    const test = await Test.findById(id).populate({ path: 'questions', populate: { path: "options" } });
     console.log(test);
-    res.render('test.ejs',{test});
+    res.render('./test/test.ejs', { test });
 })
 
 
@@ -215,8 +209,8 @@ app.get('/tests/:id',isLoggedIn,async (req,res)=>{
 // add question to test
 
 
-app.post('/tests/:id/question',isLoggedIn,async (req,res)=>{
-    const {id} = req.params;
+app.post('/tests/:id/question', isLoggedIn, async (req, res) => {
+    const { id } = req.params;
     const test = await Test.findById(id);
     const newQues = new Question(req.body);
     test.questions.push(newQues);
@@ -225,14 +219,14 @@ app.post('/tests/:id/question',isLoggedIn,async (req,res)=>{
     res.redirect(`/tests/${id}`);
 })
 
-app.delete('/tests/:testID/question/:questionID',isLoggedIn,async (req,res)=>{
-    const {testID,questionID} = req.params;
+app.delete('/tests/:testID/question/:questionID', isLoggedIn, async (req, res) => {
+    const { testID, questionID } = req.params;
     await Question.findByIdAndDelete(questionID);
     res.status(200).json('yeah it worked');
 })
 
-app.post('/test/question',isLoggedIn, async (req,res)=>{
-    const {question,correctAnswer,option} = req.body;
+app.post('/test/question', isLoggedIn, async (req, res) => {
+    const { question, correctAnswer, option } = req.body;
     const ques = new Question(req.body);
     try {
         await ques.save();
@@ -244,12 +238,14 @@ app.post('/test/question',isLoggedIn, async (req,res)=>{
 })
 
 
-app.use((err,req,res,next)=>{
-    if(err){
-        res.status(400).render("Error.ejs",{message:err.message});
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(400).render("Error.ejs", { message: err.message });
     }
 })
 
-app.all("*",(req,res)=>{
+
+
+app.all("*", (req, res) => {
     res.status(400).json('Nah bro bad request !');
 })
